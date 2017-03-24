67c6ac736c780e523ad8e2806b8fdbb63587417e
Hugues Rieublandou <hugues.rieublandou@devinci.fr>
2017-03-24 18:36:03 +0100

    added login first steps

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 var ejs = require('ejs');
 var express = require('express');
+var expressSession = require('express-session');
 var database = require('./modules/database');
-var test = require('./modules/test');
+var userSession = require('./modules/userSession');
+var bodyParser = require('body-parser');
 
 var app = express();
 
@@ -10,17 +12,33 @@ var fs = require('fs');
 var templateMain = fs.readFileSync('main.ejs', 'utf-8');
 
 app.use(express.static(__dirname + '/public'));
+app.use(expressSession({
+    secret: 'BIG_SECRET_SSH_KEY_OF_THE_DEAD_WHICH_WILL_AMAZE_YOU'
+}));
 
-app.get('/', function(req, res) {
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
+app.listen(8080);
 
 
-    var answer = database.answer
-    console.log(answer);
+app.get('/', function (req, res) {
 
-    var connect = test.connectUser("aldarick@orange.fr", "aldarick");
+    var connected = userSession.checkIfConnected(req);
+    if (connected) {
+        res.writeHead(301, {
+            Location: "http://localhost:8080/signin"
+        });
+        res.end();
+    } else {
+        res.writeHead(200, {
+            "Content-Type": "text/plain"
+        });
+        res.end("Hello World\n");
+    }
 
-    res.writeHead(200, {"Content-Type": "text/plain"});
-    res.end("Hello World\n");
 
 
 });
@@ -42,4 +60,28 @@ app.get('/main', function(req, res) {
 
 });
 
-app.listen(8080);
+app.get('/signin', function (req, res) {
+
+    var templateSignin = fs.readFileSync('sign_in.ejs', 'utf-8');
+    res.end(ejs.render(templateSignin));
+
+});
+
+app.post('/connect', function (req, res) {
+
+    var email = req.body.mail;
+
+    var password = req.body.password;
+    var correct = database.checkLoginCredentials(email, password);
+
+    if (correct) {
+        console.log("Connecting...")
+        connect(req, email, password);
+    } else {
+        res.writeHead(301, {
+            Location: "http://localhost:8080/signin"
+        });
+    }
+
+
+});
