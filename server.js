9f05d06dd3ceb5136005a6c29186084ca39c6617
Hugues Rieublandou <hugues.rieublandou@devinci.fr>
2017-03-24 16:00:37 +0100

    Added several files

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 var express = require('express');
+var database = require('./modules/database');
+var test = require('./modules/test');
 
 var app = express();
 
 app.get('/', function(req, res) {
-    res.render('./index.html');
+    
+
+    var answer = database.answer
+    console.log(answer);
+    
+    var connect = test.connectUser("aldarick@orange.fr", "aldarick");
+    
+    res.writeHead(200, {"Content-Type": "text/plain"});
+    res.end("Hello World\n");
+    
+    
 });
 
 app.listen(8080);
